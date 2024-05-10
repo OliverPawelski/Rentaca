79c38e85b91982ddcf2c6e4789940155d4b059c2
Sandra Adeyinka <sandraadeyinka@hotmail.com>
2024-05-10 14:32:47 +0100

    edit

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_09_154131) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "booking_requests", force: :cascade do |t|
+    t.integer "car_id"
+    t.integer "user_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "bookings", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
